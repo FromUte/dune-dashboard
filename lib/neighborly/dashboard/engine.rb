c9f980b13121af951929321abc005b870b513323
Josemar Luedke <josemarluedke@gmail.com>
2014-06-20 11:55:47 +0200

    Create the primary assets and add it to assets precompile list

--- lib/neighborly/dashboard/engine.rb
+++ lib/neighborly/dashboard/engine.rb
@@ -2,6 +2,10 @@ module Neighborly
   module Dashboard
     class Engine < ::Rails::Engine
       isolate_namespace Neighborly::Dashboard
+
+      initializer 'neighborly.dashboard.assets.precompile' do |app|
+        app.config.assets.precompile += %w(neighborly-dashboard-application.css neighborly-dashboard-libs.js neighborly-dashboard-application.js neighborly-dashboard-templates.js)
+      end
     end
   end
 end
