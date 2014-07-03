c31365faca4856aaec66346361e2faf317c9b5f9
Josemar Luedke <josemarluedke@gmail.com>
2014-07-03 18:08:37 +0200

    Create show boolean value helper

--- app/assets/javascripts/neighborly-dashboard-application.js
+++ app/assets/javascripts/neighborly-dashboard-application.js
@@ -5,6 +5,7 @@
 //= require_tree ./models
 //= require_tree ./i18n
 //= require_tree ./mixins
+//= require_tree ./helpers
 //= require_tree ./controllers
 //= require_tree ./views
 //= require_tree ./templates
