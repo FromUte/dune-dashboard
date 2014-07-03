66a00081b6e56604b4f681575aa1fe45af31ee4c
Josemar Luedke <josemarluedke@gmail.com>
2014-07-03 18:01:51 +0200

    Create paginated route mixin

--- app/assets/javascripts/neighborly-dashboard-application.js
+++ app/assets/javascripts/neighborly-dashboard-application.js
@@ -4,6 +4,7 @@
 //= require_tree ./initializers
 //= require_tree ./models
 //= require_tree ./i18n
+//= require_tree ./mixins
 //= require_tree ./controllers
 //= require_tree ./views
 //= require_tree ./templates
