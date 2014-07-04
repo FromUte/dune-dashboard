119e5aa4f5e0dd1debaf62f71ceff600ec9787d7
Josemar Luedke <josemarluedke@gmail.com>
2014-07-04 16:29:09 +0200

    Add bower components files

--- app/assets/javascripts/neighborly-dashboard-libs.js
+++ app/assets/javascripts/neighborly-dashboard-libs.js
@@ -2,22 +2,26 @@
 //= require handlebars
 //= require ember
 //= require ember-data
-//= require ember-auth
-//= require ember-auth-request-jquery
-//= require ember-auth-response-json
-//= require ember-auth-strategy-token
-//= require ember-auth-session-cookie
-//= require ember-auth-module-ember-data
-//= require ember-auth-module-auth-redirectable
-//= require ember-auth-module-action-redirectable
 
 //= require sidebar-tree
-//= require ember-i18n
 //= require bootstrap/dropdown
 //= require bs-core
 //= require bs-notifications
 //= require bs-breadcrumbs
 //= require pagination-pager
 //= require pagination-pager.template
-//= require numeral
-//= require moment
+
+
+// *** Bower components ***
+
+//= require ember-i18n/lib/i18n
+//= require numeral/numeral
+//= require moment/moment
+//= require ember-auth/dist/ember-auth
+//= require ember-auth-request-jquery/dist/ember-auth-request-jquery
+//= require ember-auth-response-json/dist/ember-auth-response-json
+//= require ember-auth-strategy-token/dist/ember-auth-strategy-token
+//= require ember-auth-session-cookie/dist/ember-auth-session-cookie
+//= require ember-auth-module-ember-data/dist/ember-auth-module-ember-data
+//= require ember-auth-module-auth-redirectable/dist/ember-auth-module-auth-redirectable
+//= require ember-auth-module-action-redirectable/dist/ember-auth-module-action-redirectable
