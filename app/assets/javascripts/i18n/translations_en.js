c04ae060304039048265504b444fad47d0e9a531
Josemar Luedke <josemarluedke@gmail.com>
2014-07-08 19:34:57 +0200

    Generate JS translations

--- app/assets/javascripts/i18n/translations_en.js
+++ app/assets/javascripts/i18n/translations_en.js
@@ -54,5 +54,6 @@ Ember.I18n.translations = {
   "words.save": "Save",
   "words.cancel": "Cancel",
   "words._yes": "Yes",
-  "words._no": "No"
+  "words._no": "No",
+  "words.search_results": "Search Results"
 };
