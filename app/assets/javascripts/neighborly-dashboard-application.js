7544a14111fa41c5854941ea5a3307d2b968f85b
Josemar Luedke <josemarluedke@gmail.com>
2014-07-09 20:29:12 +0200

    Create daterangepicker view and helper

--- app/assets/javascripts/neighborly-dashboard-application.js
+++ app/assets/javascripts/neighborly-dashboard-application.js
@@ -5,9 +5,9 @@
 //= require_tree ./models
 //= require_tree ./i18n
 //= require_tree ./mixins
-//= require_tree ./helpers
 //= require_tree ./controllers
 //= require_tree ./views
 //= require_tree ./templates
+//= require_tree ./helpers
 //= require_tree ./routes
 //= require ./router
